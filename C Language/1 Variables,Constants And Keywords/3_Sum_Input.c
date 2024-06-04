e4ee5c39dca5d35115b82f2e9038c13c7ae89d3b
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-04 19:09:34 +0530

    Add files via upload

---
+++ C Language/1 Variables,Constants And Keywords/3_Sum_Input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter the value of a is \n");
+    scanf("%d",&a);
+    printf("enter the value of b is \n");
+    scanf("%d",&b);
+    c=a+b;
+    printf("the sum of a and b is %d",c);
+    return 0;
+}
