e35d0dfb867bb9ec9b452e64f93e025a4d72603b
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-04 19:08:57 +0530

    Add files via upload

---
+++ C Language/1 Variables,Constants And Keywords/2_Variable.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=4;
+    // int b=4.6;     -->worng
+    float b=4.6;
+    char c='k';
+    int d=45;
+    int e=4+45;
+    printf("the value of a is %d \n",a);
+    printf("the value of b is %f \n",b);
+    printf("the value of b is %c \n",c);
+    printf("the sum of a and d %d \n",a+d);
+    printf("the sum of a and d %d \n",e);
+    return 0;
+}
