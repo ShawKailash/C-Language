50b30f68ab5abfbb92f8edb1818ff05110691f54
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-05 15:02:45 +0530

    Add files via upload

---
+++ C Language/2 Instructions And Operators/Pracitice Set/4_Solve_Question.c
@@ -0,0 +1,12 @@
+/*
+    Explain step by step evaluation of 3*x/y-z+k where x=2 y=3 z=3 k=1
+*/
+#include<stdio.h>
+
+int main()
+{
+    int x=2,y=3,z=3,k=1;
+    int result=3*x/y-z+k;
+    printf("the value of result is %d\n",result);
+    return 0;
+}
