a5705af6809dd8d4cf2c837b6b8e268d52f6a7c1
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-05 15:06:18 +0530

    Add files via upload

---
+++ C Language/2 Instructions And Operators/1_Type_Declarations_Instructions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    b=c=a=4;
+    printf("the value of a is %d\n",a);
+    printf("the value of b is %d\n",b);
+    printf("the value of c is %d\n",c);
+    printf("\n");
+    // float e=d+8.9;  -->error
+    float d=1.1;
+    float e=d+8.9;
+    printf("the value of e is %f\n",e);
+
+    return 0;
+}
