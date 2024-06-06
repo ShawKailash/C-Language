4c354b759561d0588f0c6585fe6e1f121bc8de61
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-06 17:14:33 +0530

    Add files via upload

---
+++ C Language/3 Conditional Instructions/Practice Set/1_Output.c
@@ -0,0 +1,27 @@
+/*
+    What will be the output of this program
+    int a=10;
+    if(a=11)
+    {
+        printf("i am 11");
+    }
+    else
+    {
+        printf("i am not 11");
+    }
+*/
+#include<stdio.h>
+
+int main()
+{
+    int a=10;
+    if(a=11)
+    {
+        printf("i am 11\n");
+    }
+    else
+    {
+        printf("i am not 11\n");
+    }
+    return 0;
+}
