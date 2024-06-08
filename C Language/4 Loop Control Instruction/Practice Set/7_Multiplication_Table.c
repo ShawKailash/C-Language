4742448433f8e2133bf478a4893bc7440cea2aa1
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-08 11:12:27 +0530

    Add files via upload

---
+++ C Language/4 Loop Control Instruction/Practice Set/7_Multiplication_Table.c
@@ -0,0 +1,14 @@
+/*
+    write a program to calculate the sum of the numbers
+    occuring in the multiplication table of 8
+*/
+#include<stdio.h>
+
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+        printf("8 x %d = %d\n",i,8*i);
+    }
+    return 0;
+}
