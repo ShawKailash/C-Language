623aad8920c54dbbf21dc033ca5b7f5e9d7b18c6
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-08 11:02:18 +0530

    Add files via upload

---
+++ C Language/4 Loop Control Instruction/1_While_Loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter the number is \n");
+    scanf("%d",&a);
+    while(a<10)
+    {
+        printf("%d\n",a);
+        a++;
+    }
+    return 0;
+}
