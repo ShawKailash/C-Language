e256b012cc5f4de9270fbbdd7af92da16f68040d
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-09 09:23:43 +0530

    Add files via upload

---
+++ C Language/4 Loop Control Instruction/7_Quick_Quiz.c
@@ -0,0 +1,14 @@
+/*
+    write a program to print first n natural number using for loop
+*/
+#include<stdio.h>
+
+int main()
+{
+    for(int i=0;i<3;i++)
+    {
+        printf("%d",i);
+        printf("\n");
+    }
+    return 0;
+}
