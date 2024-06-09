8a1dee6402291b511a53ba35709fe0d4977c76ef
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-09 09:22:59 +0530

    Add files via upload

---
+++ C Language/4 Loop Control Instruction/6_For_Loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter the number is\n");
+    scanf("%d",&a);
+    for(a;a<10;a++)
+    {
+        printf("%d\n",a+1);
+    }
+    return 0;
+}
