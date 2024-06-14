7a56819a3d86c1ca9437290297d1de47d180a3b6
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-06-14 18:20:34 +0530

    Add files via upload

---
+++ C Language/7 Arrays/9_Quick_Quiz.c
@@ -0,0 +1,14 @@
+/*
+    create a 2D array by taking input from the user
+    write a display function to print the content of
+    this 2D array an the screen
+*/
+#include<stdio.h>
+
+int main()
+{
+    int n_subject=3;
+    int n_marks=2;
+    
+    return 0;
+}
